fda3e715886d38b72f7f8dbf39a8b60016b4668b
Gordon Watts <gordon@gordonandpaula.net>
2019-11-24 16:21:21 -0800

    First go at URL parsing
    Tests, even, that work!

---
+++ src/app/indico/url-cleaning.service.spec.ts
@@ -0,0 +1,48 @@
+import { TestBed } from '@angular/core/testing';
+
+import { UrlCleaningService } from './url-cleaning.service';
+
+describe('UrlCleaningService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: UrlCleaningService = TestBed.get(UrlCleaningService);
+    expect(service).toBeTruthy();
+  });
+
+  it('CERN site returns itself', () => {
+    const service: UrlCleaningService = TestBed.get(UrlCleaningService);
+    let actual = service.get_canonical_url("https://indico.cern.ch/event/799542/");
+    expect(actual).toEqual("https://indico.cern.ch/event/799542");
+  });
+
+  it('CERN site returns itself no trailing slash', () => {
+    const service: UrlCleaningService = TestBed.get(UrlCleaningService);
+    let actual = service.get_canonical_url("https://indico.cern.ch/event/799542");
+    expect(actual).toEqual("https://indico.cern.ch/event/799542");
+  });
+
+  it('CERN site Export Returns event', () => {
+    const service: UrlCleaningService = TestBed.get(UrlCleaningService);
+    let actual = service.get_canonical_url("https://indico.cern.ch/export/event/799542.json");
+    expect(actual).toEqual("https://indico.cern.ch/event/799542");
+  });
+  
+  it('CERN site contribution', () => {
+    const service: UrlCleaningService = TestBed.get(UrlCleaningService);
+    let actual = service.get_canonical_url("https://indico.cern.ch/event/799542/contributions/3489878/");
+    expect(actual).toEqual("https://indico.cern.ch/event/799542");
+  });
+
+  it('Site Down a Level', () => {
+    const service: UrlCleaningService = TestBed.get(UrlCleaningService);
+    let actual = service.get_canonical_url("https://indico.cern.ch/subdir/event/799542/contributions/3489878/");
+    expect(actual).toEqual("https://indico.cern.ch/subdir/event/799542");
+  });
+
+  it('non-indico site', () => {
+    const service: UrlCleaningService = TestBed.get(UrlCleaningService);
+    let actual = service.get_canonical_url("https://nytimes.com");
+    expect(actual).toEqual("https://nytimes.com");
+  });
+});
--- src/app/indico/url-cleaning.service.ts
+++ src/app/indico/url-cleaning.service.ts
@@ -9,7 +9,43 @@ export class UrlCleaningService {
 
   // Return a URL that represents a meeting, as best we can.
   // This does not go a network check - this is purely syntax.
-  public get_canonical_url(url: string) : string {
-    return url;
+  // The indico url looks like 'https://node/x/y/z/event/xxxx/blah' where
+  // x,y, and z might be sub-directories. And z could be export. Everything after
+  // blah doesn't matter.
+  // If we can't figure out the url then we return it.
+  public get_canonical_url(url_string: string) : string {
+    let url = new URL(url_string);
+
+    // Find the 'event' string to figure out where we are looking.
+    // If there is no event, then we will have to bail.
+    let path_components = url.pathname.split('/').filter(c => c.length > 0);
+
+    let event_index = path_components.indexOf('event');
+    if (event_index < 0) {
+      return url_string;
+    }
+
+    // If the previous guy is "export", then we drop that.
+    if (event_index != 0 && path_components[event_index-1] === "export") {
+      path_components.splice(event_index-1, 1);
+      event_index = event_index - 1;
+    }
+
+    // Drop everything after the event ID.
+    if (path_components.length > event_index+2) {
+      path_components.splice(event_index+2, path_components.length - event_index - 2);
+    }
+
+    // Now, if the event ID is anything funny, fix it up.
+    let event_id = path_components[path_components.length-1];
+    let dot_index = event_id.indexOf('.');
+    if (dot_index >= 0) {
+      path_components[path_components.length-1] = event_id.substring(0, dot_index);
+    }
+
+    // And restore it.
+    url.pathname = path_components.join('/');
+
+    return url.toString();
   }
 }
